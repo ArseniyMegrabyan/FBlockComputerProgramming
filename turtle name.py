413d02c61f0cabb55b815186e78cf8009ffcf41c
Arseniy Megrabyan <arseniy.megrabyan@eaglebrook.org>
2022-01-24 14:00:44 -0500

    jan24

---
+++ turtle name.py
@@ -0,0 +1,28 @@
+from turtle import *
+penup()
+# backward(500)
+left(180)
+forward(500)
+pendown()
+right(120)
+forward(100)
+right(120)
+forward(100)
+left(180)
+forward(50)
+left(60)
+forward(50)
+left(60)
+forward(45)
+left(120)
+penup()
+forward(120)
+pendown()
+left(90)
+forward(90)
+
+
+
+
+
+done()
