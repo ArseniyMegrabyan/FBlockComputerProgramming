fff7a2ee548aa1f3009fa865e7b66d42a390e280
Arseniy Megrabyan <arseniy.megrabyan@eaglebrook.org>
2022-02-08 10:21:09 -0500

    feb8.py

--- jan21.py
+++ jan21.py
@@ -1,11 +1,11 @@
 from turtle import *
 
 
-for x in range(50):
+for x in range(20):
     forward(200)
     left(140)
 left(140)
 forward(200)
-fastest(100)
+fastest(10)
 
 done()
---
+++ loop.py
@@ -0,0 +1,15 @@
+from turtle import *
+
+for x in range(9):
+ forward(50)
+ left(100)
+ forward(50)
+ right(60)
+ forward(50)
+
+for x in range(1):
+ forward(50)
+ right(90)
+forward(300)
+
+done()
