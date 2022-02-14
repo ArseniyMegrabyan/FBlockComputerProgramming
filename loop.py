fe6640e644126fb304e999a6fd1546affb03e5fe
Arseniy Megrabyan <arseniy.megrabyan@eaglebrook.org>
2022-02-14 11:12:28 -0500

    feb14.py

--- loop.py
+++ loop.py
@@ -1,15 +1,42 @@
 from turtle import *
 
-for x in range(9):
- forward(50)
- left(100)
- forward(50)
- right(60)
- forward(50)
+pencolor("red")
 
-for x in range(1):
- forward(50)
- right(90)
-forward(300)
+def sawtooth( distance, angle):
+    forward(distance)
+    left(angle)
+    forward(distance)
+
+
+
+
+# for x in range(9):
+#  forward(50)
+#  left(100)
+#  forward(50)
+ 
+#  right(60)
+#  forward(50)
+
+# pencolor("blue")
+# for x in range(2):
+#  forward(300)
+#  right(90)
+#  forward(300)
+
+#  right(90)
+#  forward(300)
+
+# pencolor("green")
+# for x in range(10):
+#  forward(15)
+#  pencolor("orange")
+#  forward(15)
+#  pencolor("purple")
+# right(90)
+
+for x in range (50):
+    sawtooth(50, 160)
+    sawtooth(50, 160)
 
 done()
--- sus.py
+++ sus.py
@@ -1,6 +1,45 @@
-from turtle import turtle
+
+from turtle import Turtle
 
 
+ben = Turtle()
+racetrack = Turtle()
+screen = ben.getscreen()
+distance = 0
+angle = 10
+update = 0.2
+
+def accelerate():
+    global distance
+    distance+=update
+
+def decelerate():
+    global distance
+    distance-=update
+
+def moveleft():
+    ben.left(angle)
+
+def moveright():
+    ben.right(angle)
+
+def startdrawing(x, y):
+    racetrack.goto(x,y)
+    racetrack.pendown()
 
 
+screen.listen()
+screen.onkeypress(accelerate, "w")
+screen.onkeypress(decelerate, "s")
+screen.onkeypress(moveright,"d")
+screen.onkeypress(moveleft,"a")
+racetrack.onclick(startdrawing)
+racetrack.ondrag(racetrack.goto)
+racetrack.onrelease(racetrack.penup)
 
+# ##Draw racetrack
+# racetrack.forward()
+
+while True:
+    ben.forward(distance)
+    screen.update()
